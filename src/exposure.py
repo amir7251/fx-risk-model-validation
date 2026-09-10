36c7c879e1f6f1de3b3162337d34ba3db09e6a7e
amir7251 <amir05077@outlook.com>
2026-09-10 18:56:37 +0100

    Add known-loss check for USD P&L

--- src/exposure.py
+++ src/exposure.py
@@ -10,8 +10,16 @@ current_usd_value = calculate_usd_value(pounds_held, current_exchange_rate)
 def calculate_usd_pnl(previous_value, current_value):
     return current_value - previous_value
 usd_pnl = calculate_usd_pnl(previous_usd_value, current_usd_value)
+loss_test_actual = calculate_usd_pnl(200000, 195000)
+loss_test_expected = -5000
+loss_test_passed = loss_test_actual == loss_test_expected
 
 print('previous USD value:', previous_usd_value)
 print('current USD value:', current_usd_value)
 print('USD P&L:', usd_pnl)
-
+print('loss test actual:', loss_test_actual)
+print('loss test expected:', loss_test_expected)
+if loss_test_passed:
+    print('loss test passed')
+else:
+    print('loss test failed')
