6144fe1b469b7ca6d4d18f42b119513594488527
amir7251 <amir05077@outlook.com>
2026-09-10 18:43:42 +0100

    Add reusable USD P&L calculation function

--- src/exposure.py
+++ src/exposure.py
@@ -6,7 +6,10 @@ def calculate_usd_value(gbp_amount, exchange_rate):
     return gbp_amount * exchange_rate
 previous_usd_value = calculate_usd_value(pounds_held, previous_exchange_rate)
 current_usd_value = calculate_usd_value(pounds_held, current_exchange_rate)
-usd_pnl = current_usd_value - previous_usd_value
+
+def calculate_usd_pnl(previous_value, current_value):
+    return current_value - previous_value
+usd_pnl = calculate_usd_pnl(previous_usd_value, current_usd_value)
 
 print('previous USD value:', previous_usd_value)
 print('current USD value:', current_usd_value)
