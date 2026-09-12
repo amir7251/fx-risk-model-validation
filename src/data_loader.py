46dd547fad6a440396f194a3aa5a536f27b45298
amir7251 <amir05077@outlook.com>
2026-09-12 17:36:30 +0100

    Convert non-empty exchange rates to numeric values

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,6 +1,7 @@
 import csv
 
 exchange_rates = []
+numeric_rates = []
 
 with open ('data/raw/gbpusd_daily.csv', newline='') as file:
     reader = csv.reader(file)
@@ -16,11 +17,23 @@ with open ('data/raw/gbpusd_daily.csv', newline='') as file:
         if exchange_rate == '':
             missing_rate_count = missing_rate_count + 1
         else:
-            non_missing_rate_count_rate_count = non_missing_rate_count = non_missing_rate_count + 1
+            non_missing_rate_count = non_missing_rate_count + 1
+            numeric_rate = float(exchange_rate)
+            numeric_rates.append(numeric_rate)
 
 number_of_rows = len(exchange_rates)
-count_check = number_of_rows == missing_rate_count + non_missing_rate_count_rate_count
+count_check = number_of_rows == missing_rate_count + non_missing_rate_count
+number_numeric_rates = len(numeric_rates)
+
+
 print('number of rows:', number_of_rows)
 print('missing rate count:', missing_rate_count)
-print('valid rate count:', non_missing_rate_count_rate_count)
-print('valid count check:', count_check)
+print('non-missing rate count:', non_missing_rate_count)
+print('row count check:', count_check)
+    
+print('number of numeric rates:', number_numeric_rates)
+print('first numeric rate:', numeric_rates[0])
+if non_missing_rate_count == number_numeric_rates:
+    print('the number of numeric rates matches the non missing rate count')
+else:
+    print('the number of numeric rates do not match the non missing rate count')
