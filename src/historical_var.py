49d0243d11be0e91c1295eb8e8c87d78106ae579
amir7251 <amir05077@outlook.com>
2026-09-14 14:13:07 +0100

    Compare initial historical threshold with next observed return

--- src/historical_var.py
+++ src/historical_var.py
@@ -22,6 +22,11 @@ sorted_returns = sorted(window_returns)
 
 return_threshold = sorted_returns[24]
 
+next_observation = dated_returns[500]
+actual_return = next_observation[2]
+
+breach = actual_return < return_threshold
+
 print('window size:', len(historical_window))
 print('first observation in window:', historical_window[0])
 print('last observation in window;', historical_window[-1])
@@ -29,5 +34,11 @@ print('number of sorted returns:', len(sorted_returns))
 print('lowest return (%):', sorted_returns[0] * 100)
 print('highest return (%):', sorted_returns[-1] * 100)
 print('95% return threshold (%):', return_threshold * 100)
+
+print('forecast period started:', next_observation[0])
+print('forecast period ended;', next_observation[1])
+print('actual return (%):', actual_return * 100)
+print('95% threshold breached:', breach)
+
 print('number of returns:', len(dated_returns))
 print('first return:', dated_returns[0])
