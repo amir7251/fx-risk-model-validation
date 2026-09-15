8c594c62c6337c30d789e7eca93db6602b164da7
amir7251 <amir05077@outlook.com>
2026-09-15 22:27:44 +0100

    Add rolling 500-observation historical VaR backtest

--- src/historical_var.py
+++ src/historical_var.py
@@ -1,7 +1,7 @@
 import csv
 
 dated_returns = []
-window_returns = []
+
 
 with open('data/processed/gbpusd_returns.csv', newline='') as file:
     reader = csv.reader(file)
@@ -14,18 +14,23 @@ with open('data/processed/gbpusd_returns.csv', newline='') as file:
 
         dated_returns.append([previous_date, current_date, daily_return])
 
-historical_window = dated_returns[:500]
 
-for observation in historical_window:
-    window_returns.append(observation[2])
-sorted_returns = sorted(window_returns)
+backtest_results = []
+for index in range(500, len(dated_returns)):
+    historical_window = dated_returns[index - 500:index]
+    window_returns = []
 
-return_threshold = sorted_returns[24]
+    for observation in historical_window:
+        window_returns.append(observation[2])
+    sorted_returns = sorted(window_returns)
+    return_threshold = sorted_returns[24]
+    next_observation = dated_returns[index]
+    actual_return = next_observation[2]
+    breach = actual_return < return_threshold
+    backtest_results.append([next_observation[0], next_observation[1], return_threshold, actual_return, breach])
 
-next_observation = dated_returns[500]
-actual_return = next_observation[2]
+        
 
-breach = actual_return < return_threshold
 
 print('window size:', len(historical_window))
 print('first observation in window:', historical_window[0])
@@ -42,3 +47,5 @@ print('95% threshold breached:', breach)
 
 print('number of returns:', len(dated_returns))
 print('first return:', dated_returns[0])
+print('number of backtest results:', len(backtest_results))
+print('first backtest result:', backtest_results[0])
