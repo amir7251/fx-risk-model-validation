853ef5f4c127308d632b50d3f2034d91fcddde2f
amir7251 <amir05077@outlook.com>
2026-09-17 21:50:45 +0100

    Standardise returns using EWMA volatility estimates

--- src/filtered_historical_var.py
+++ src/filtered_historical_var.py
@@ -25,6 +25,21 @@ for daily_return in initial_returns:
 initial_variance = sum(squared_returns) / len(squared_returns)
 initial_volatility = math.sqrt(initial_variance)
 
+decay_factor = 0.94
+variance_estimate = initial_variance
+standardised_returns = []
+
+for observation in dated_returns[500:]:
+    daily_return = observation[2]
+    volatility_estimate = math.sqrt(variance_estimate)
+    standardised_return = daily_return / volatility_estimate
+    standardised_returns.append(standardised_return)
+    variance_estimate = (decay_factor * variance_estimate + (1- decay_factor) * daily_return ** 2)
+
+
+
 print('number of returns:', len(dated_returns))
 print('initial variance:', initial_variance)
 print('initial volatility (%):', initial_volatility * 100)
+print('number of standardised returns:', len(standardised_returns))
+print('first standardised return:', standardised_returns[0])
