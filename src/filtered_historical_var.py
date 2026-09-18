a5e7a9f3407238b2210d75aa3949a7d8aa56c272
amir7251 <amir05077@outlook.com>
2026-09-18 13:22:05 +0100

    Add rolling filtered historical VaR backtest at 95% and 99%

--- src/filtered_historical_var.py
+++ src/filtered_historical_var.py
@@ -28,18 +28,36 @@ initial_volatility = math.sqrt(initial_variance)
 decay_factor = 0.94
 variance_estimate = initial_variance
 standardised_returns = []
+volatility_estimates = []
+filtered_backtest_results = []
 
 for observation in dated_returns[500:]:
     daily_return = observation[2]
     volatility_estimate = math.sqrt(variance_estimate)
+    volatility_estimates.append(volatility_estimate)
     standardised_return = daily_return / volatility_estimate
     standardised_returns.append(standardised_return)
     variance_estimate = (decay_factor * variance_estimate + (1- decay_factor) * daily_return ** 2)
 
-
+for index in range(500, len(standardised_returns)):
+    historical_window = standardised_returns[index - 500:index]
+    sorted_returns = sorted(historical_window)
+    standardised_threshold_95 = sorted_returns[24]
+    standardised_threshold_99 = sorted_returns[4]
+    forecast_volatility = volatility_estimates[index]
+    return_threshold_95 = standardised_threshold_95 * forecast_volatility
+    return_threshold_99 = standardised_threshold_99 * forecast_volatility
+    next_observation = dated_returns[index + 500]
+    actual_return = next_observation[2]
+    breach_95 = actual_return < return_threshold_95
+    breach_99 = actual_return < return_threshold_99
+    filtered_backtest_results.append([next_observation[0], next_observation[1], return_threshold_95, actual_return, breach_95, return_threshold_99, breach_99])
 
 print('number of returns:', len(dated_returns))
 print('initial variance:', initial_variance)
 print('initial volatility (%):', initial_volatility * 100)
 print('number of standardised returns:', len(standardised_returns))
 print('first standardised return:', standardised_returns[0])
+print('number of filtered backtest results:', len(filtered_backtest_results))
+print('first filtered result:', filtered_backtest_results[0])
+print('last filtered result:', filtered_backtest_results[-1])
